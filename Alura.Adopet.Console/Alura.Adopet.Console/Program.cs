d4846329c823c00638f724e55fcccb5771638525
bessax <andrebessax@gmail.com>
2023-02-14 17:10:30 -0300

    refactor: ajuste na exibição da função show.

--- Alura.Adopet.Console/Alura.Adopet.Console/Program.cs
+++ Alura.Adopet.Console/Alura.Adopet.Console/Program.cs
@@ -67,16 +67,19 @@ try
             }
             Console.ReadKey();
             break;
-        case "show":       
+        case "show":
+            
                 using (StreamReader sr = new StreamReader(args[1]))
                 {
+                    Console.WriteLine("----- Importaddos os dados abaixo -----");
                     while (!sr.EndOfStream)
                     {
                         string[] propriedades = sr.ReadLine().Split(';');
                         Pet pet = new Pet(Guid.Parse(propriedades[0]),
                         propriedades[1],
                         TipoPet.Cachorro
-                        );  
+                        );
+                        Console.WriteLine(pet);
                     }
                 } 
             Console.ReadKey();
