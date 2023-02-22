4f55241f62e9b1e735d73efd8cb3127b8ae92daa
Daniel Portugal <dpcosta@gmail.com>
2023-02-22 16:00:01 -0300

    Melhorando a legibilidade do código através da extração de métodos.

--- Alura.Adopet.Console/Alura.Adopet.Console/Program.cs
+++ Alura.Adopet.Console/Alura.Adopet.Console/Program.cs
@@ -2,88 +2,101 @@
 using System.Net.Http.Json;
 using Alura.Adopet.Console;
 
+async Task Import()
+{
+    List<Pet> listaDePet = new List<Pet>();
+
+    using (StreamReader sr = new StreamReader(args[1]))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] propriedades = sr.ReadLine().Split(';');
+            Pet pet = new Pet(Guid.Parse(propriedades[0]),
+              propriedades[1],
+              TipoPet.Cachorro
+             );
+
+            Console.WriteLine(pet);
+            listaDePet.Add(pet);
+        }
+    }
+    foreach (var pet in listaDePet)
+    {
+        try
+        {
+            var resposta = await CreatePetAsync(pet);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    Console.WriteLine("Importação concluída!");
+    Console.ReadKey();
+}
+
+void Help()
+{
+    Console.WriteLine("Lista de comandos.");
+    if (args.Length == 1)
+    {
+        Console.WriteLine("adopet help <parametro> ous simplemente adopet help  " +
+             "comando que exibe informações de ajuda dos comandos.");
+        Console.WriteLine("Adopet (1.0) - Aplicativo de linha de comando (CLI).");
+        Console.WriteLine("Realiza a importação em lote de um arquivos de pets.");
+        Console.WriteLine("Comando possíveis: ");
+        Console.WriteLine($" adopet import <arquivo> comando que realiza a importação do arquivo de pets.");
+        Console.WriteLine($" adopet show   <arquivo> comando que exibe no terminal o conteúdo do arquivo importado." + "\n\n\n\n");
+        Console.WriteLine("Execute 'adopet.exe help [comando]' para obter mais informações sobre um comando." + "\n\n\n");
+    }
+    else if (args.Length == 2)
+    {
+        if (args[1].Equals("import"))
+        {
+            Console.WriteLine(" adopet import <arquivo> " +
+                "comando que realiza a importação do arquivo de pets.");
+        }
+        if (args[1].Equals("show"))
+        {
+            Console.WriteLine(" adopet show <arquivo>  comando que " +
+                "exibe no terminal o conteúdo do arquivo importado.");
+        }
+    }
+    Console.ReadKey();
+}
+
+void Show()
+{
+    using (StreamReader sr = new StreamReader(args[1]))
+    {
+        Console.WriteLine("----- Importaddos os dados abaixo -----");
+        while (!sr.EndOfStream)
+        {
+            string[] propriedades = sr.ReadLine().Split(';');
+            Pet pet = new Pet(Guid.Parse(propriedades[0]),
+            propriedades[1],
+            TipoPet.Cachorro
+            );
+            Console.WriteLine(pet);
+        }
+    }
+    Console.ReadKey();
+}
+
 HttpClient client = ConfiguraHttpClient("https://localhost:7136");
 Console.ForegroundColor = ConsoleColor.Green;
 try
 {
-   
     switch (args[0].Trim())
     {
-        case "import":               
-               List<Pet> listaDePet = new List<Pet>();
-                
-                using (StreamReader sr = new StreamReader(args[1]))
-                {                  
-                    while (!sr.EndOfStream)
-                    {
-                        string[] propriedades = sr.ReadLine().Split(';');
-                        Pet pet = new Pet(Guid.Parse(propriedades[0]),
-                          propriedades[1],
-                          TipoPet.Cachorro
-                         );
-
-                    Console.WriteLine(pet);
-                        listaDePet.Add(pet);                        
-                    }
-                }
-                foreach (var pet in listaDePet)
-                {
-                    try
-                    {
-                        var resposta = await CreatePetAsync(pet);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }                    
-                 }                
-                Console.WriteLine("Importação concluída!");
-                Console.ReadKey();          
+        case "import":
+            await Import();
             break;
         case "help":
-            Console.WriteLine("Lista de comandos.");
-            if (args.Length == 1)
-            {
-                Console.WriteLine("adopet help <parametro> ous simplemente adopet help  " +
-                     "comando que exibe informações de ajuda dos comandos.");
-                Console.WriteLine("Adopet (1.0) - Aplicativo de linha de comando (CLI).");
-                Console.WriteLine("Realiza a importação em lote de um arquivos de pets.");
-                Console.WriteLine("Comando possíveis: ");
-                Console.WriteLine($" adopet import <arquivo> comando que realiza a importação do arquivo de pets.");
-                Console.WriteLine($" adopet show   <arquivo> comando que exibe no terminal o conteúdo do arquivo importado." + "\n\n\n\n");
-                Console.WriteLine("Execute 'adopet.exe help [comando]' para obter mais informações sobre um comando." + "\n\n\n");
-            }
-            else if (args.Length==2)
-            {
-                if (args[1].Equals("import"))
-                {
-                    Console.WriteLine(" adopet import <arquivo> " +
-                        "comando que realiza a importação do arquivo de pets.");
-                }
-                if (args[1].Equals("show"))
-                {
-                    Console.WriteLine(" adopet show <arquivo>  comando que " +
-                        "exibe no terminal o conteúdo do arquivo importado.");
-                }
-            }
-            Console.ReadKey();
+            Help();
             break;
         case "show":
-            
-                using (StreamReader sr = new StreamReader(args[1]))
-                {
-                    Console.WriteLine("----- Importaddos os dados abaixo -----");
-                    while (!sr.EndOfStream)
-                    {
-                        string[] propriedades = sr.ReadLine().Split(';');
-                        Pet pet = new Pet(Guid.Parse(propriedades[0]),
-                        propriedades[1],
-                        TipoPet.Cachorro
-                        );
-                        Console.WriteLine(pet);
-                    }
-                } 
-            Console.ReadKey();
+            Show();
             break;
         default:
             Console.WriteLine("Comando inválido!");
