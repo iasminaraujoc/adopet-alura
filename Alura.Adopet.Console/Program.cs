7c64f44df5305d405b2a54e6f18db73cd77abc62
Daniel Portugal <dpcosta@gmail.com>
2023-02-28 11:59:21 -0300

    Classe Program com tudo entulhado nela. E sem as classes que representam comandos.

--- Alura.Adopet.Console/Program.cs
+++ Alura.Adopet.Console/Program.cs
@@ -1,34 +1,89 @@
-﻿using Alura.Adopet.Console;
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Alura.Adopet.Console;
 
-const string IMPORT = "import";
-const string HELP = "help";
-const string SHOW = "show";
-
+HttpClient client = ConfiguraHttpClient("https://localhost:7136");
 Console.ForegroundColor = ConsoleColor.Green;
 try
 {
-    string comando = args[0];
 
-    switch (comando.Trim())
+    switch (args[0].Trim())
     {
-        case IMPORT:
-            var import = new Import();
-            await import.RealizaImportacaoAsync(caminhoArquivo:args[1]);
-            break;
-        case HELP:
-            var help = new Help();
-            if (args.Length == 2) 
+        case "import":
+            List<Pet> listaDePet = new List<Pet>();
+
+            using (StreamReader sr = new StreamReader(args[1]))
             {
-                help.HelpDoComando(docComando: args[1]);
+                while (!sr.EndOfStream)
+                {
+                    string[] propriedades = sr.ReadLine().Split(';');
+                    Pet pet = new Pet(Guid.Parse(propriedades[0]),
+                      propriedades[1],
+                      TipoPet.Cachorro
+                     );
+
+                    Console.WriteLine(pet);
+                    listaDePet.Add(pet);
+                }
             }
-            else
+            foreach (var pet in listaDePet)
             {
-                help.ExibeDocumentacao();
+                try
+                {
+                    var resposta = await CreatePetAsync(pet);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
+            Console.WriteLine("Importação concluída!");
+            Console.ReadKey();
             break;
-        case SHOW:
-            var show = new Show();
-            show.ExibeArquivo(caminhoArquivo:args[1]);
+        case "help":
+            Console.WriteLine("Lista de comandos.");
+            if (args.Length == 1)
+            {
+                Console.WriteLine("adopet help <parametro> ous simplemente adopet help  " +
+                     "comando que exibe informações de ajuda dos comandos.");
+                Console.WriteLine("Adopet (1.0) - Aplicativo de linha de comando (CLI).");
+                Console.WriteLine("Realiza a importação em lote de um arquivos de pets.");
+                Console.WriteLine("Comando possíveis: ");
+                Console.WriteLine($" adopet import <arquivo> comando que realiza a importação do arquivo de pets.");
+                Console.WriteLine($" adopet show   <arquivo> comando que exibe no terminal o conteúdo do arquivo importado." + "\n\n\n\n");
+                Console.WriteLine("Execute 'adopet.exe help [comando]' para obter mais informações sobre um comando." + "\n\n\n");
+            }
+            else if (args.Length == 2)
+            {
+                if (args[1].Equals("import"))
+                {
+                    Console.WriteLine(" adopet import <arquivo> " +
+                        "comando que realiza a importação do arquivo de pets.");
+                }
+                if (args[1].Equals("show"))
+                {
+                    Console.WriteLine(" adopet show <arquivo>  comando que " +
+                        "exibe no terminal o conteúdo do arquivo importado.");
+                }
+            }
+            Console.ReadKey();
+            break;
+        case "show":
+
+            using (StreamReader sr = new StreamReader(args[1]))
+            {
+                Console.WriteLine("----- Importaddos os dados abaixo -----");
+                while (!sr.EndOfStream)
+                {
+                    string[] propriedades = sr.ReadLine().Split(';');
+                    Pet pet = new Pet(Guid.Parse(propriedades[0]),
+                    propriedades[1],
+                    TipoPet.Cachorro
+                    );
+                    Console.WriteLine(pet);
+                }
+            }
+            Console.ReadKey();
             break;
         default:
             Console.WriteLine("Comando inválido!");
@@ -43,4 +98,23 @@ catch (Exception ex)
 finally
 {
     Console.ForegroundColor = ConsoleColor.White;
+}
+
+HttpClient ConfiguraHttpClient(string url)
+{
+    HttpClient _client = new HttpClient();
+    _client.DefaultRequestHeaders.Accept.Clear();
+    _client.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue("application/json"));
+    _client.BaseAddress = new Uri(url);
+    return _client;
+}
+
+Task<HttpResponseMessage> CreatePetAsync(Pet pet)
+{
+    HttpResponseMessage? response = null;
+    using (response = new HttpResponseMessage())
+    {
+        return client.PostAsJsonAsync("pet/add", pet);
+    }
 }
