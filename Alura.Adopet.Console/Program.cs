85b56ca13c5e34de13eebec3bd6f599453a4191d
bessax <andrebessax@gmail.com>
2023-03-01 16:41:05 -0300

    #1.4 Usando parâmetro nomeado.
    Divisão do comando help em 2 métodos, e para aumentar a legibilidade usamos paraâmetros nomeados.

--- Alura.Adopet.Console/Program.cs
+++ Alura.Adopet.Console/Program.cs
@@ -8,16 +8,24 @@ Console.ForegroundColor = ConsoleColor.Green;
 try
 {
     // args[0] é o comando a ser executado pelo programa
+
     switch (args[0].Trim())
     {
         case "import":
-            await Import();            
+            await Import(caminhoArquivo: args[1]);            
             break;
         case "help":
-            Help();            
+            if (args.Length == 2)       
+            {
+                HelpDoComando(comando: args[1]);
+            }     
+            else
+            {
+                Help();
+            }
             break;
         case "show":
-            Show();            
+            Show(caminhoArquivo: args[1]);            
             break;
         case "list":
             await List();            
@@ -64,12 +72,12 @@ async Task<IEnumerable<Pet>?> ListPetsAsync()
     return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
 }
 
-async Task Import()
+async Task Import(string caminhoArquivo)
 {
     List<Pet> listaDePet = new List<Pet>();
 
     // args[1] é o caminho do arquivo a ser importado
-    using (StreamReader sr = new StreamReader(args[1]))
+    using (StreamReader sr = new StreamReader(caminhoArquivo))
     {
         while (!sr.EndOfStream)
         {
@@ -104,43 +112,39 @@ void Help()
 {
     Console.WriteLine("Lista de comandos.");
     // se não passou mais nenhum argumento mostra help de todos os comandos
-    if (args.Length == 1)
-    {
-        Console.WriteLine("adopet help <parametro> ous simplemente adopet help  " +
-             "comando que exibe informações de ajuda dos comandos.");
-        Console.WriteLine("Adopet (1.0) - Aplicativo de linha de comando (CLI).");
-        Console.WriteLine("Realiza a importação em lote de um arquivos de pets.");
-        Console.WriteLine("Comando possíveis: ");
-        Console.WriteLine($" adopet import <arquivo> comando que realiza a importação do arquivo de pets.");
-        Console.WriteLine($" adopet show   <arquivo> comando que exibe no terminal o conteúdo do arquivo importado." + "\n\n\n\n");
-        Console.WriteLine("Execute 'adopet.exe help [comando]' para obter mais informações sobre um comando." + "\n\n\n");
-    }
-    // exibe o help daquele comando específico
-    else if (args.Length == 2)
-    {
-        if (args[1].Equals("import"))
+    Console.WriteLine("adopet help <parametro> ous simplemente adopet help  " +
+            "comando que exibe informações de ajuda dos comandos.");
+    Console.WriteLine("Adopet (1.0) - Aplicativo de linha de comando (CLI).");
+    Console.WriteLine("Realiza a importação em lote de um arquivos de pets.");
+    Console.WriteLine("Comando possíveis: ");
+    Console.WriteLine($" adopet import <arquivo> comando que realiza a importação do arquivo de pets.");
+    Console.WriteLine($" adopet show   <arquivo> comando que exibe no terminal o conteúdo do arquivo importado." + "\n\n\n\n");
+    Console.WriteLine("Execute 'adopet.exe help [comando]' para obter mais informações sobre um comando." + "\n\n\n");
+    Console.ReadKey();
+}
+
+void HelpDoComando(string comando)
+{
+        if (comando.Equals("import"))
         {
             Console.WriteLine(" adopet import <arquivo> " +
                 "comando que realiza a importação do arquivo de pets.");
         }
-        if (args[1].Equals("show"))
+        if (comando.Equals("show"))
         {
             Console.WriteLine(" adopet show <arquivo>  comando que " +
                 "exibe no terminal o conteúdo do arquivo importado.");
         }
-        if (args[1].Equals("list"))
+        if (comando.Equals("list"))
         {
             Console.WriteLine(" adopet list  comando que " +
                 "exibe no terminal o conteúdo importado no servidor.");
         }
-    }
-    Console.ReadKey();
 }
-
-void Show()
+void Show(string caminhoArquivo)
 {
     // args[1] é o caminho do arquivo a ser exibido
-    using (StreamReader sr = new StreamReader(args[1]))
+    using (StreamReader sr = new StreamReader(caminhoArquivo))
     {
         Console.WriteLine("----- Importaddos os dados abaixo -----");
         while (!sr.EndOfStream)
