2f378360c36b41c3dde3f9e285715585d16e3ba5
bessax <andrebessax@gmail.com>
2023-03-21 16:49:41 -0300

    #refact: Separada a configuração do Swagger em m métdo de extensão.

--- Alura.Adopet.API/Program.cs
+++ Alura.Adopet.API/Program.cs
@@ -1,11 +1,9 @@
 using Alura.Adopet.API.Controladores;
 using Alura.Adopet.API.Dados.Context;
-using Alura.Adopet.API.Dominio.Dto;
 using Alura.Adopet.API.Service.Interface;
 using Alura.Adopet.API.Startup;
-using Alura.Adopet.API.Validations;
 using APICatalogo.Extensions;
-using FluentValidation;
+
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);// Criando uma aplicação Web.
@@ -22,34 +20,23 @@ builder.Services.AddControllers().AddNewtonsoftJson(options =>
    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
 );
 
-//DI
 builder.Services.ConfigureDI();
 builder.Services.AddAutoMapper(typeof(Program));
 
-//Habilitando o swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //Adicionando serviços.
 var serviceProvider = builder.Services.BuildServiceProvider();
 var eventoService = serviceProvider.GetService<IEventoService>();
-
 //Gerando dados fake.
 var app = builder.Build();
 eventoService.GenerateFakeDate();
 
-// Ativando o Swagger
 app.UseSwagger();
 app.AddEndpointsPet();
 app.AddEndpointsCliente();
-// Ativando a interface Swagger
-app.UseSwaggerUI(
-    c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoAPI V1");
-        c.RoutePrefix = string.Empty;
-    }
-);
+app.ConfigureSwaggerApp();
 app.ConfigureExceptionHandler();
-// Roda a aplicação
+
 app.Run();
---
+++ Alura.Adopet.API/Startup/ConfigureSwagger.cs
@@ -0,0 +1,17 @@
+﻿namespace Alura.Adopet.API.Startup
+{
+    public static class ConfigureSwagger
+    {
+        public static void ConfigureSwaggerApp(this IApplicationBuilder app)
+        {
+            // Ativando a interface Swagger
+            app.UseSwaggerUI(
+                c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoAPI V1");
+                    c.RoutePrefix = string.Empty;
+                }
+            );
+        }
+    }
+}
