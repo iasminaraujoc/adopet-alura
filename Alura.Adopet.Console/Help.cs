412ba9207e17862bd9050e1940349d6889254617
laris <larissagabriela@ufrj.br>
2023-04-03 17:19:05 -0300

    #EXTRA Desafio

--- Alura.Adopet.Console/Help.cs
+++ Alura.Adopet.Console/Help.cs
@@ -8,5 +8,39 @@ namespace Alura.Adopet.Console
 {
     internal class Help
     {
+        public void Documentacao()
+        {
+            System.Console.WriteLine("Lista de comandos.");
+            System.Console.WriteLine("adopet help <parametro> ous simplemente adopet help  " +
+                    "comando que exibe informações de ajuda dos comandos.");
+            System.Console.WriteLine("Adopet (1.0) - Aplicativo de linha de comando (CLI).");
+            System.Console.WriteLine("Realiza a importação em lote de um arquivos de pets.");
+            System.Console.WriteLine("Comando possíveis: ");
+            System.Console.WriteLine($" adopet list   comando que exibe no terminal os pets cadastrados no banco da AdoPet.");
+            System.Console.WriteLine($" adopet import <arquivo> comando que realiza a importação do arquivo de pets.");
+            System.Console.WriteLine($" adopet show   <arquivo> comando que exibe no terminal o conteúdo do arquivo importado." + "\n\n\n\n");
+            System.Console.WriteLine("Execute 'adopet.exe help [comando]' para obter mais informações sobre um comando." + "\n\n\n");
+
+        }
+
+        public void HelpDoComando(string comando)
+        {
+            if (comando.Equals("import"))
+            {
+                System.Console.WriteLine(" adopet import <arquivo> " +
+                    "comando que realiza a importação do arquivo de pets.");
+            }
+            if (comando.Equals("show"))
+            {
+                System.Console.WriteLine(" adopet show <arquivo>  comando que " +
+                    "exibe no terminal o conteúdo do arquivo importado.");
+            }
+            if (comando.Equals("list"))
+            {
+                System.Console.WriteLine($" adopet list   comando que " +
+                    "exibe no terminal os pets cadastrados no banco da AdoPet.");
+
+            }
+        }
     }
 }
